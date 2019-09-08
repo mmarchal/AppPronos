d55e0ce1ace48a2474a06c8ab2ec0624a438fb9a
mmarchal <marchal.maxime@live.fr>
2019-09-08 16:50:45 +0200

    Sociaux

--- lib/sociaux.dart
+++ lib/sociaux.dart
@@ -9,11 +9,50 @@ class Sociaux extends StatefulWidget {
 
 class _Sociaux extends State<Sociaux> {
 
+  List<Map> listeReseaux = [
+    {'nom' : 'Facebook', 'photo' : 'assets/reseaux/facebook.png', 'couleur' : Colors.blue[600]},
+    {'nom' : 'Twitter', 'photo' : 'assets/reseaux/twitter.png', 'couleur' : Colors.red},
+    {'nom' : 'Instagram', 'photo' : 'assets/reseaux/instagram.png', 'couleur' : Colors.green},
+    {'nom' : 'Mail', 'photo' : 'assets/reseaux/mail.jpg', 'couleur' : Colors.orange },
+  ];
+
+  List<Widget> generateButtons() {
+
+    List liste = new List();
+    listeReseaux.forEach((value){
+      Widget test = new RaisedButton(
+          onPressed: () {
+            print('${value['nom']} cliqué');
+          },
+          color: value['couleur'],
+          shape: new RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(50.0)),
+          child: new Container(
+            width: MediaQuery.of(context).size.width/2,
+            padding: EdgeInsets.all(5.0),
+            child: new Row(
+              children: <Widget>[
+                new Image.asset(value['photo'], width: 50, height: 50,),
+                new Padding(padding: EdgeInsets.only(left: 20.0)),
+                new Text(value['nom'], textAlign: TextAlign.center, textScaleFactor: 1.5, style: TextStyle(color: Colors.white),)
+              ],
+            )
+          )
+      );
+      liste.add(test);
+    });
+    return liste;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Center(
-        child: Text("Coucou")
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: generateButtons(),
+        )
       )
     );
   }
